52582f075f7e017cc082919558eeb8e776214b42
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-24 18:21:29 -0500

    Edited postal codes

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -4,9 +4,9 @@ VALUES (1, 'Sterling Archer', 'dutchess@isis.com', '$2a$10$FB/BOAVhpuLvpOREQVmvm
 (3, 'Mallory Archer', 'absinthe@isis.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties(id, owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active )
-VALUES (1, 3, 'Office', 'Spy Agency', 'img/office_thumb.png', 'img/office.png', 800, 20, 10, 4, 'United States', 'New York Street', 'New York', 'New York', 'askdjalks', TRUE),
-(2, 3, 'Apartment', 'High end dwelling', 'img/apartment_thumb.png', 'img/apartment.png', 1000, 2, 2, 2, 'Canada', 'Truckasaurus Street', 'Ottawa', 'Ontario', 'askdjalks', TRUE),
-(3, 3, 'Penthouse', 'Affluent Living Space', 'img/penthouse_thumb.png', 'img/penthouse.png', 3000, 5, 5, 5, 'Tunisia', 'Martini Street', 'Bourbon', 'Ben Arous', 'askdjalks', TRUE);
+VALUES (1, 3, 'Office', 'Spy Agency', 'img/office_thumb.png', 'img/office.png', 800, 20, 10, 4, 'United States', 'New York Street', 'New York', 'New York', 'asdfhgdrt', TRUE),
+(2, 3, 'Apartment', 'High end dwelling', 'img/apartment_thumb.png', 'img/apartment.png', 1000, 2, 2, 2, 'Canada', 'Truckasaurus Street', 'Ottawa', 'Ontario', 'asdfhgdrt', TRUE),
+(3, 3, 'Penthouse', 'Affluent Living Space', 'img/penthouse_thumb.png', 'img/penthouse.png', 3000, 5, 5, 5, 'Tunisia', 'Martini Street', 'Bourbon', 'Ben Arous', 'asdfhgdrt', TRUE);
 
 INSERT INTO reservations (guest_id, property_id, start_date, end_date)
 VALUES (1, 3, '2018-10-14', '2018-11-27'),
